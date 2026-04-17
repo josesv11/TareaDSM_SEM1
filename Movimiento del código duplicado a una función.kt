9483c0fc779a70e2b4d31c2933de256a3d4b57e8
josesv11 <143985741+josesv11@users.noreply.github.com>
2026-04-17 16:55:41 -0500

    Add files via upload

---
+++ Movimiento del código duplicado a una función.kt
@@ -0,0 +1,10 @@
+fun main() {
+    printWeatherForCity()
+}
+
+fun printWeatherForCity() {
+    println("City: Ankara")
+    println("Low temperature: 27, High temperature: 31")
+    println("Chance of rain: 82%")
+    println()
+}
